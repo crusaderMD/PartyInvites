aaca8f2f26d1082608a590d71b366bbb711f081c
Luiz R. Bortolotti Jr. <luiz_bortolotti@hotmial.com>
2024-12-27 13:22:31 -0300

    Adicionada captura dos dados do formulario, exibição dos dados gravados (lista), validação dos dados no formulário e estilo css

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,10 +11,29 @@ namespace PartyInvites.Controllers
             return View();
         }
 
+        [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
         }
 
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
     }
 }
