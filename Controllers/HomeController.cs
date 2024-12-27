c3062bafcf91b6c7c4aa0d62d5f0f9b582c422cf
Luiz R. Bortolotti Jr. <luiz_bortolotti@hotmial.com>
2024-12-27 09:22:29 -0300

    Commit Inicial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
